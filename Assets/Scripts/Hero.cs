6d4b9cd2bcb791eca4291a29d540b0e7077bdba1
RIBOUT <pifi77@hotmail.fr>
2020-10-03 23:13:33 +0200

    [Movement] Move work

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public bool leftDirection = false;
+    public bool bounceBack = false;
+    [Range(0.1f,5f)]
+    public float size = 1f;
+    public Vector2 minMaxSize = new Vector2(0.2f, 2f);
+
+    public Vector2 currentTangent = new Vector2(0, 1);
+    public float accumulateur = 0;
+
+    public bool moveAuthorize = true;
+    public float rotationSpeed = 1f;
+    //public List<GameObject> debugObj = new List<GameObject>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputManagement();
+        MovementManagement();
+    }
+
+    public void InputManagement()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            leftDirection = !leftDirection;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+
+        }
+    }
+
+    public void MovementManagement()
+    {
+        float newAccumulateur = accumulateur + Time.deltaTime * (leftDirection ? -1 : 1) * (bounceBack ? -1 : 1) * rotationSpeed * (1/size);
+        Vector2 newTangent;
+        newTangent.x = Mathf.Sin(newAccumulateur);
+        newTangent.y = Mathf.Cos(newAccumulateur);
+        newTangent.Normalize();
+
+        Vector2 centerPosition;
+        if (leftDirection) {
+            centerPosition = new Vector2(-currentTangent.y, currentTangent.x).normalized * size;
+        }
+        else
+        {
+            centerPosition = new Vector2(currentTangent.y, -currentTangent.x).normalized * size;
+        }
+        
+        //debugObj[1].transform.position = (Vector2)this.transform.position + centerPosition;
+
+        Vector2 falseObjectif =Vector3.Project(centerPosition, newTangent);
+        
+        Vector2 directionFromCenter = falseObjectif - centerPosition;
+        directionFromCenter = directionFromCenter.normalized * size;
+
+        Vector2 newObjectif = centerPosition + directionFromCenter;
+
+        //debugObj[0].transform.position = (Vector2)this.transform.position + newObjectif;
+
+        Debug.DrawLine(transform.position, (Vector2)this.transform.position + newObjectif, Color.green);
+        Debug.DrawLine((Vector2)this.transform.position + newObjectif, (Vector2)this.transform.position + centerPosition, Color.green);
+        Debug.DrawLine((Vector2)this.transform.position + centerPosition, transform.position, Color.green);
+
+        if (moveAuthorize)
+        {
+            currentTangent = newTangent;
+            accumulateur = newAccumulateur;
+
+            this.transform.Translate(newObjectif);
+        }
+
+        PreviewManager();
+    }
+
+
+    public void PreviewManager()
+    {
+        //particule ?
+    }
+}
---
+++ Assets/Scripts/Palette.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyBox;
+
+public class Palette : MonoBehaviour
+{
+    [System.Serializable]
+    public struct colourList
+    {
+        public string id;
+        public Color background;
+        public Color hero;
+        public Color heroOtherSens;
+        public Color heroLeft;
+        public Color heroRight;
+        public Color ennemi;
+        public Color obstacle;
+        public Color wall;
+    }
+
+    public List<colourList> paletteList = new List<colourList>();
+    public int currentPalette = 0;
+
+
+    [MyBox.ButtonMethod]
+    public void ReColorAll()
+    {
+        if (currentPalette < 0 || currentPalette >= paletteList.Count)
+            return;
+
+        colourList curre = paletteList[currentPalette];
+        foreach (SpriteRenderer sR in FindObjectsOfType<SpriteRenderer>())
+        {
+            if (sR.name.Contains("colHero"))
+            {
+                sR.color = curre.hero;
+            }
+            if (sR.name.Contains("colLef"))
+            {
+                sR.color = curre.heroLeft;
+            }
+            if (sR.name.Contains("colRig"))
+            {
+                sR.color = curre.heroRight;
+            }
+            if (sR.name.Contains("colEnn"))
+            {
+                sR.color = curre.ennemi;
+            }
+            if (sR.name.Contains("colObs"))
+            {
+                sR.color = curre.obstacle;
+            }
+            if (sR.name.Contains("colWall"))
+            {
+                sR.color = curre.wall;
+            }
+        }
+
+        FindObjectOfType<Camera>().backgroundColor = curre.background;
+    }
+}
